fd93dec90ee7c71ca70b0d6687e85d34850e22f3
mostafijrana1 <mostafijrana1@gmail.com>
2019-10-15 12:50:24 +0600

    update

---
+++ assets/nasim_bazar/js/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+/* for mobile menu */
+	$('#menu').slicknav();
+/* for mix it up */
+    $('.project_list').mixItUp();
+	
+/*=============== for mega menu section ================== */
+
+/* /* for menu hover 
+$(".menu_area .menu_bar ul li").hover(function(){
+	$(".menu_area .menu_bar .mega_menu").css("display", "block");
+	},function(){
+	$(".menu_area .menu_bar .mega_menu").css("display", "none");	
+});
+
+ /* for mega menu hover 
+$(".menu_area .menu_bar .mega_menu").hover(function(){
+	$(".menu_area .menu_bar .mega_menu").css("display", "block");
+	},function(){
+	$(".menu_area .menu_bar .mega_menu").css("display", "none");
+}); */
+
+
+
+
+
+});
---
+++ assets/nasim_bazar/readme.txt
@@ -0,0 +1,9 @@
+boilarplate (5.0.0) -- Done!
+bootstrap (3.3.2)   -- Done!
+fontawesome (4.3.0) -- Done!
+slicknav mobile     -- Done!
+Html5 Shiv          -- Done!
+less framework      -- Done!
+
+respond js       -- null! (for ie)(cross browser)
+
